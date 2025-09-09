7ee807c4199f2b295255bcbbee7c3f8f68feaff7
sandhyrajawat <sandhyarajawat1100@gmail.com>
2025-09-09 11:44:55 +0530

    few changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,53 +30,8 @@
       </a>
     </div>
   </div>
-  <ul class="list-unstyled" style="margin-bottom: 0">
-    <li class="media post-form w-shadow">
-      <div class="media-body">
-        <div class="form-group post-input">
-          <textarea
-            class="form-control"
-            id="postForm"
-            rows="2"
-            placeholder="What's on your mind, Arthur?"></textarea>
-        </div>
-        <div class="row post-form-group">
-          <div class="col-md-9">
-            <button
-              type="button"
-              class="btn btn-link post-form-btn btn-sm">
-              <img
-                src="assets/images/icons/theme/post-image.png"
-                alt="post form icon" />
-              <span>Photo/Video</span>
-            </button>
-            <button
-              type="button"
-              class="btn btn-link post-form-btn btn-sm">
-              <img
-                src="assets/images/icons/theme/tag-friend.png"
-                alt="post form icon" />
-              <span>Tag Friends</span>
-            </button>
-            <button
-              type="button"
-              class="btn btn-link post-form-btn btn-sm">
-              <img
-                src="assets/images/icons/theme/check-in.png"
-                alt="post form icon" />
-              <span>Check In</span>
-            </button>
-          </div>
-          <div class="col-md-3 text-right">
-            <button type="button" class="btn btn-primary btn-sm">
-              Publish
-            </button>
-          </div>
-        </div>
-      </div>
-    </li>
-  </ul>
-
+  @include('layouts.post-form')
+  
   <!-- Posts -->
   <div class="posts-section mb-5">
     <div class="post border-bottom p-3 bg-white w-shadow">
---
+++ resources/views/layouts/post-form.blade.php
@@ -0,0 +1,46 @@
+<ul class="list-unstyled" style="margin-bottom: 0">
+    <li class="media post-form w-shadow">
+      <div class="media-body">
+        <div class="form-group post-input">
+          <textarea
+            class="form-control"
+            id="postForm"
+            rows="2"
+            placeholder="What's on your mind, Arthur?"></textarea>
+        </div>
+        <div class="row post-form-group">
+          <div class="col-md-9">
+            <button
+              type="button"
+              class="btn btn-link post-form-btn btn-sm">
+              <img
+                src="assets/images/icons/theme/post-image.png"
+                alt="post form icon" />
+              <span>Photo/Video</span>
+            </button>
+            <button
+              type="button"
+              class="btn btn-link post-form-btn btn-sm">
+              <img
+                src="assets/images/icons/theme/tag-friend.png"
+                alt="post form icon" />
+              <span>Tag Friends</span>
+            </button>
+            <button
+              type="button"
+              class="btn btn-link post-form-btn btn-sm">
+              <img
+                src="assets/images/icons/theme/check-in.png"
+                alt="post form icon" />
+              <span>Check In</span>
+            </button>
+          </div>
+          <div class="col-md-3 text-right">
+            <button type="button" class="btn btn-primary btn-sm">
+              Publish
+            </button>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
