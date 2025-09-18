f02dba65518c63c93650670b9bbc09640ef67f9f
Pradeep Singh <bhanwarpsrathore@gmail.com>
2025-09-18 16:52:04 +0530

    wip

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -3,394 +3,94 @@
 @section('content')
 
 <div class="col-md-8 second-section" id="page-content-wrapper">
-  <!-- Quick Links -->
-  <div class="mb-3">
-    <div class="btn-group d-flex bg-red-900">
-      <!-- ...buttons... -->
-    </div>
-  </div>
-  <!-- Post Form -->
-
-  <div class="col-md-12 second-section" id="page-content-wrapper">
+    <!-- Quick Links -->
     <div class="mb-3">
-      <div class="btn-group d-flex">
-        <a
-          href="index.html"
-          class="btn btn-quick-links mr-3 ql-active">
-          <img
-            src="assets/images/icons/theme/speech.png"
-            class="mr-2"
-            alt="quick links icon" />
-          <span class="fs-8">Speech</span>
-        </a>
-        <a href="messages.html" class="btn btn-quick-links mr-3">
-          <img
-            src="assets/images/icons/theme/listen.png"
-            class="mr-2"
-            alt="quick links icon" />
-          <span class="fs-8">Listen</span>
-        </a>
-        <a href="watch.html" class="btn btn-quick-links">
-          <img
-            src="assets/images/icons/theme/watch.png"
-            class="mr-2"
-            alt="quick links icon" />
-          <span class="fs-8">Watch</span>
-        </a>
-      </div>
-    </div>
-    <!-- Post Form  -->
-    <ul class="list-unstyled" style="margin-bottom: 0">
-      <li class="media post-form w-shadow">
-        <div class="media-body">
-          <div class="form-group post-input">
-            <textarea
-              class="form-control"
-              id="postForm"
-              rows="2"
-              placeholder="What's on your mind, Arthur?"></textarea>
-          </div>
-          <div class="row post-form-group">
-            <div class="col-md-9">
-              <button
-                type="button"
-                class="btn btn-link post-form-btn btn-sm">
-                <img
-                  src="assets/images/icons/theme/post-image.png"
-                  alt="post form icon" />
-                <span>Photo/Video</span>
-              </button>
-              <button
-                type="button"
-                class="btn btn-link post-form-btn btn-sm">
-                <img
-                  src="assets/images/icons/theme/tag-friend.png"
-                  alt="post form icon" />
-                <span>Tag Friends</span>
-              </button>
-              <button
-                type="button"
-                class="btn btn-link post-form-btn btn-sm">
-                <img
-                  src="assets/images/icons/theme/check-in.png"
-                  alt="post form icon" />
-                <span>Check In</span>
-              </button>
-            </div>
-            <div class="col-md-3 text-right">
-              <button type="button" class="btn btn-primary btn-sm">
-                Publish
-              </button>
-            </div>
-          </div>
+        <div class="btn-group d-flex bg-red-900">
+            <!-- ...buttons... -->
         </div>
-      </li>
-    </ul>
+    </div>
+    <!-- Post Form -->
 
-  </div>
-
-  <div class="post border-bottom p-3 bg-white w-shadow">
-    <div class="media text-muted pt-3">
-      <img
-        src="assets/images/users/user-1.jpg"
-        alt="Online user"
-        class="mr-3 post-user-image" />
-      <div class="media-body pb-3 mb-0 small lh-125">
-        <div
-          class="d-flex justify-content-between align-items-center w-100">
-          <a href="#" class="text-gray-dark post-user-name">John Michael</a>
-          <div class="dropdown">
-            <a
-              href="#"
-              class="post-more-settings"
-              role="button"
-              data-toggle="dropdown"
-              id="postOptions"
-              aria-haspopup="true"
-              aria-expanded="false">
-              <i class="bx bx-dots-horizontal-rounded"></i>
-            </a>
-            <div
-              class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left post-dropdown-menu">
-              <a
-                href="#"
-                class="dropdown-item"
-                aria-describedby="savePost">
-                <div class="row">
-                  <div class="col-md-2">
-                    <i
-                      class="bx bx-bookmark-plus post-option-icon"></i>
-                  </div>
-                  <div class="col-md-10">
-                    <span class="fs-9">Save post</span>
-                    <small
-                      id="savePost"
-                      class="form-text text-muted">Add this to your saved items</small>
-                  </div>
-                </div>
-              </a>
-              <a
-                href="#"
-                class="dropdown-item"
-                aria-describedby="hidePost">
-                <div class="row">
-                  <div class="col-md-2">
-                    <i class="bx bx-hide post-option-icon"></i>
-                  </div>
-                  <div class="col-md-10">
-                    <span class="fs-9">Hide post</span>
-                    <small
-                      id="hidePost"
-                      class="form-text text-muted">See fewer posts like this</small>
-                  </div>
-                </div>
-              </a>
-              <a
-                href="#"
-                class="dropdown-item"
-                aria-describedby="snoozePost">
-                <div class="row">
-                  <div class="col-md-2">
-                    <i class="bx bx-time post-option-icon"></i>
-                  </div>
-                  <div class="col-md-10">
-                    <span class="fs-9">Snooze Lina for 30 days</span>
-                    <small
-                      id="snoozePost"
-                      class="form-text text-muted">Temporarily stop seeing posts</small>
-                  </div>
-                </div>
-              </a>
-              <a
-                href="#"
-                class="dropdown-item"
-                aria-describedby="reportPost">
-                <div class="row">
-                  <div class="col-md-2">
-                    <i class="bx bx-block post-option-icon"></i>
-                  </div>
-                  <div class="col-md-10">
-                    <span class="fs-9">Report</span>
-                    <small
-                      id="reportPost"
-                      class="form-text text-muted">I'm concerned about this post</small>
-                  </div>
-                </div>
-              </a>
+    <div class="col-md-12 second-section" id="page-content-wrapper">
+        <div class="mb-3">
+            <div class="btn-group d-flex">
+                <a
+                    href="index.html"
+                    class="btn btn-quick-links mr-3 ql-active">
+                    <img
+                        src="assets/images/icons/theme/speech.png"
+                        class="mr-2"
+                        alt="quick links icon" />
+                    <span class="fs-8">Speech</span>
+                </a>
+                <a href="messages.html" class="btn btn-quick-links mr-3">
+                    <img
+                        src="assets/images/icons/theme/listen.png"
+                        class="mr-2"
+                        alt="quick links icon" />
+                    <span class="fs-8">Listen</span>
+                </a>
+                <a href="watch.html" class="btn btn-quick-links">
+                    <img
+                        src="assets/images/icons/theme/watch.png"
+                        class="mr-2"
+                        alt="quick links icon" />
+                    <span class="fs-8">Watch</span>
+                </a>
             </div>
-          </div>
         </div>
-        <span class="d-block">3 hours ago <i class="bx bx-globe ml-3"></i></span>
-      </div>
-    </div>
-    <div class="mt-3">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis voluptatem veritatis harum, tenetur, quibusdam voluptatum, incidunt saepe minus maiores ea atque sequi illo veniam sint quaerat corporis totam et. Culpa?
-      </p>
-    </div>
-    <div class="d-block mt-3">
-      <img
-        src="https://scontent.fevn1-2.fna.fbcdn.net/v/t1.0-9/56161887_588993861570433_2896723195090436096_n.jpg?_nc_cat=103&_nc_eui2=AeFI0UuTq3uUF_TLEbnZwM-qSRtgOu0HE2JPwW6b4hIki73-2OWYhc7L1MPsYl9cYy-w122CCak-Fxj0TE1a-kjsd-KXzh5QsuvxbW_mg9qqtg&_nc_ht=scontent.fevn1-2.fna&oh=ea44bffa38f368f98f0553c5cef8e455&oe=5D050B05"
-        class="post-content"
-        alt="post image" />
-    </div>
-    <div class="mb-3">
-      <!-- Reactions -->
-      <div class="argon-reaction">
-        <span class="like-btn">
-          <a href="#" class="post-card-buttons" id="reactions"><i class="bx bxs-like mr-2"></i> 67</a>
-
-        </span>
-      </div>
-      <a
-        href="javascript:void(0)"
-        class="post-card-buttons"
-        id="show-comments"><i class="bx bx-message-rounded mr-2"></i> 5</a>
-      <div class="dropdown dropup share-dropup">
-        <a
-          href="#"
-          class="post-card-buttons"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false">
-          <i class="bx bx-share-alt mr-2"></i> Share
-        </a>
-      </div>
-    </div>
-    <!-- pp -->
-    <div
-      class="border-top pt-3 hide-comments">
-      <div class="row bootstrap snippets">
-        <div class="col-md-12">
-          <div class="comment-wrapper">
-            <div class="panel panel-info">
-              <div class="panel-body">
-                <ul class="media-list comments-list">
-                  <li class="media comment-form">
-                    <a href="#" class="pull-left">
-                      <img
-                        src="assets/images/users/user-4.jpg"
-                        alt=""
-                        class="img-circle" />
-                    </a>
-                    <div class="media-body">
-                      <form action="" method="" role="form">
-                        <div class="row">
-                          <div class="col-md-12">
-                            <div class="input-group">
-                              <input
-                                type="text"
-                                class="form-control comment-input"
-                                placeholder="Write a comment..." />
-
-                              <div class="input-group-btn">
-                                <button
-                                  type="button"
-                                  class="btn comment-form-btn"
-                                  data-toggle="tooltip"
-                                  data-placement="top"
-                                  title="Tooltip on top">
-                                  <i
-                                    class="bx bxs-smiley-happy"></i>
-                                </button>
-                                <button
-                                  type="button"
-                                  class="btn comment-form-btn comment-form-btn"
-                                  data-toggle="tooltip"
-                                  data-placement="top"
-                                  title="Tooltip on top">
-                                  <i class="bx bx-camera"></i>
-                                </button>
-                                <button
-                                  type="button"
-                                  class="btn comment-form-btn comment-form-btn"
-                                  data-toggle="tooltip"
-                                  data-placement="top"
-                                  title="Tooltip on top">
-                                  <i class="bx bx-microphone"></i>
-                                </button>
-                                <button
-                                  type="button"
-                                  class="btn comment-form-btn"
-                                  data-toggle="tooltip"
-                                  data-placement="top"
-                                  title="Tooltip on top">
-                                  <i class="bx bx-file-blank"></i>
-                                </button>
-                              </div>
-                            </div>
-                          </div>
+        <!-- Post Form  -->
+        <ul class="list-unstyled" style="margin-bottom: 0">
+            <li class="media post-form w-shadow">
+                <div class="media-body">
+                    <div class="form-group post-input">
+                        <textarea
+                            class="form-control"
+                            id="postForm"
+                            rows="2"
+                            placeholder="What's on your mind, Arthur?"></textarea>
+                    </div>
+                    <div class="row post-form-group">
+                        <div class="col-md-9">
+                            <button
+                                type="button"
+                                class="btn btn-link post-form-btn btn-sm">
+                                <img
+                                    src="assets/images/icons/theme/post-image.png"
+                                    alt="post form icon" />
+                                <span>Photo/Video</span>
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-link post-form-btn btn-sm">
+                                <img
+                                    src="assets/images/icons/theme/tag-friend.png"
+                                    alt="post form icon" />
+                                <span>Tag Friends</span>
+                            </button>
+                            <button
+                                type="button"
+                                class="btn btn-link post-form-btn btn-sm">
+                                <img
+                                    src="assets/images/icons/theme/check-in.png"
+                                    alt="post form icon" />
+                                <span>Check In</span>
+                            </button>
                         </div>
-                      </form>
+                        <div class="col-md-3 text-right">
+                            <button type="button" class="btn btn-primary btn-sm">
+                                Publish
+                            </button>
+                        </div>
                     </div>
-                  </li>
-                  <li class="media">
-                    <a href="#" class="pull-left">
-                      <img
-                        src="assets/images/users/user-2.jpg"
-                        alt=""
-                        class="img-circle" />
-                    </a>
-                    <div class="media-body">
-                      <div
-                        class="d-flex justify-content-between align-items-center w-100">
-                        <strong class="text-gray-dark"><a href="#" class="fs-8">Karen Minas</a></strong>
-                        <a href="#"><i
-                            class="bx bx-dots-horizontal-rounded"></i></a>
-                      </div>
-                      <span class="d-block comment-created-time">30 min ago</span>
-                      <p class="fs-8 pt-2">
-                        Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Lorem ipsum dolor sit
-                        amet,
-                        <a href="#">#consecteturadipiscing </a>.
-                      </p>
-                      <div class="commentLR">
-                        <button
-                          type="button"
-                          class="btn btn-link fs-8">
-                          Like
-                        </button>
-                        <button
-                          type="button"
-                          class="btn btn-link fs-8">
-                          Reply
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                  <li class="media">
-                    <a href="#" class="pull-left">
-                      <img
-                        src="https://bootdey.com/img/Content/user_2.jpg"
-                        alt=""
-                        class="img-circle" />
-                    </a>
-                    <div class="media-body">
-                      <div
-                        class="d-flex justify-content-between align-items-center w-100">
-                        <strong class="text-gray-dark"><a href="#" class="fs-8">Lia Earnest</a></strong>
-                        <a href="#"><i
-                            class="bx bx-dots-horizontal-rounded"></i></a>
-                      </div>
-                      <span class="d-block comment-created-time">2 hours ago</span>
-                      <p class="fs-8 pt-2">
-                        Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Lorem ipsum dolor sit
-                        amet,
-                        <a href="#">#consecteturadipiscing </a>.
-                      </p>
-                      <div class="commentLR">
-                        <button
-                          type="button"
-                          class="btn btn-link fs-8">
-                          Like
-                        </button>
-                        <button
-                          type="button"
-                          class="btn btn-link fs-8">
-                          Reply
-                        </button>
-                      </div>
-                    </div>
-                  </li>
+                </div>
+            </li>
+        </ul>
 
-                  <li class="media">
-                    <div class="media-body">
-                      <div class="comment-see-more text-center">
-                        <button
-                          type="button"
-                          class="btn btn-link fs-8">
-                          See More
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-  <div class="d-block mt-3">
-    <video
-      id="my_video_1"
-      class="video-js vjs-default-skin post-content"
-      width="640px"
-      height="400px"
-      controls
-      preload="none"
-      poster="https://scontent.fevn1-2.fna.fbcdn.net/v/t1.0-9/53323455_587990788367325_4529827897430507520_n.jpg?_nc_cat=100&_nc_eui2=AeF-F6s-7bevnyjZs6JbGj3WPqHUIRKQ4uJ2vH8L-OD-3KZPZFJ7GVOVSYewqLB1928c3NeJ-OWQgN9et1oxB4kpONH0rFMSpp1H-lfjwH2tzA&_nc_ht=scontent.fevn1-2.fna&oh=ab0d869caefae1260b3ff755e2e031ba&oe=5D07FF32"
-      data-setup='{ "aspectRatio":"640:400", "playbackRates": [1, 1.5, 2] }'>
-      <source
-        src="https://vjs.zencdn.net/v/oceans.mp4"
-        type="video/mp4" />
-      <source
-        src="https://vjs.zencdn.net/v/oceans.webm"
-        type="video/webm" />
-    </video>
 
+    <!-- Posts -->
+    <div id="posts-section" class="posts-section mb-5" id="post">
 
-    @endsection
+    </div>
+</div>
